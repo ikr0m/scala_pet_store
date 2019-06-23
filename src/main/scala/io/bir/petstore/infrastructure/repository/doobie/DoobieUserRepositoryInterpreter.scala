ec1aa789cb77f79f905c0f529b0979cd4fd3a396
Ikrom <ikromur@gmail.com>
2019-06-23 15:59:47 +0500

    PetValidationInterpreter

--- src/main/scala/io/bir/petstore/infrastructure/repository/doobie/DoobieUserRepositoryInterpreter.scala
+++ src/main/scala/io/bir/petstore/infrastructure/repository/doobie/DoobieUserRepositoryInterpreter.scala
@@ -51,3 +51,38 @@ private object UserSQL {
       FROM USERS
     """.query
 }
+
+class DoobieUserRepositoryInterpreter[F[_]: Bracket[?[_], Throwable]](val xa: Transactor[F])
+extends UserRepositoryAlgebra[F]
+with IdentityStore[F, Long, User] { self =>
+  import UserSQL._
+
+  def create(user: User): F[User] =
+    insert(user).withUniqueGeneratedKeys[Long]("ID").map(id => user.copy(id = id.some)).transact(xa)
+
+  def update(user: User): OptionT[F, User] =
+    OptionT.fromOption[F](user.id).semiflatMap { id =>
+      UserSQL.update(user, id).run.transact(xa).as(user)
+    }
+
+  def get(userId: Long): OptionT[F, User] = OptionT(select(userId).option.transact(xa))
+
+  def findByUserName(userName: String): OptionT[F, User] =
+    OptionT(byUserName(userName).option.transact(xa))
+
+  def delete(userId: Long): OptionT[F, User] = get(userId).semiflatMap(user =>
+    UserSQL.delete(userId).run.transact(xa).as(user)
+  )
+
+  def deleteByUserName(userName: String): OptionT[F, User] =
+    findByUserName(userName).mapFilter(_.id).flatMap(delete)
+
+  def list(pageSize: Int, offset: Int): F[List[User]] =
+    paginate(pageSize, offset)(selectAll).to[List].transact(xa)
+}
+
+object DoobieUserRepositoryInterpreter {
+  def apply[F[_]: Bracket[?[_], Throwable]](xa: Transactor[F]): DoobieUserRepositoryInterpreter[F] =
+    new DoobieUserRepositoryInterpreter(xa)
+}
+
